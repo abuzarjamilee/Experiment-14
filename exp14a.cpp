cd908eb87904869108b68d9e1f51f8c8323ab948
abuzarjamilee <abuzarjamilee@gmail.com>
2024-10-23 01:43:57 +0530

    Add files via upload

---
+++ exp14a.cpp
@@ -0,0 +1,30 @@
+// Abuzar Jamilee
+// 23070123158
+
+# include <iostream>
+# include <string>
+using namespace std;
+
+class uni
+{
+    public:
+    string uni = "Symbiosis";
+    void course()
+    {
+        cout<<"B. Tech ";
+    }
+};
+class branch: public uni
+{
+    public:
+    string branch = " Electronics and Telecommunication ";
+
+};
+
+int main()
+{
+    branch b1;
+    cout<<b1.uni<<endl;
+    b1.course();
+    cout<<b1.branch;
+}
